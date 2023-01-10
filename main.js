4ffc7331a14e2b8357cc2d58c1bf0c2c8e2ed615
Natallia2107 <105502657+Natallia2107@users.noreply.github.com>
2023-01-10 13:09:34 +0100

    eighth task is done

--- main.js
+++ main.js
@@ -110,3 +110,12 @@ const alpha = people.sort((lastOne, nextOne) => {
 
 // reduce exercise - sum up the instances od each od these 
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck']
+
+const transportation = data.reduce(function(obj, item) {
+    if (!obj[item]) {
+        obj[item] = 0
+    } 
+    obj[item]++
+    return obj
+}, {})
+console.log(transportation);
